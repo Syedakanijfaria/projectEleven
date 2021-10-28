cfae11357166a010baaffcdbdf45931ba9b100d3
SKFaria <fariafaria737@gmail.com>
2021-10-29 02:07:24 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlTJFMeb3V0pVA1ZxQSp1QFyrL2YYegPo",
+    authDomain: "tourism-website-client.firebaseapp.com",
+    projectId: "tourism-website-client",
+    storageBucket: "tourism-website-client.appspot.com",
+    messagingSenderId: "423022676759",
+    appId: "1:423022676759:web:525f0d0997c6649087c552"
+};
+
+export default firebaseConfig;
