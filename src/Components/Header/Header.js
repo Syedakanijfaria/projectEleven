1a473faa3b02055861643a763f5f9ba877aeec42
SKFaria <fariafaria737@gmail.com>
2021-10-29 12:59:36 +0600

    client

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import './Header.css';
 
 const Header = () => {
@@ -9,17 +10,25 @@ const Header = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <a className="navbar-brand" href="#">Extreme Traveler</a>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Home</a>
+                    <a className="navbar-brand" href="#">
+                        <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+                        Extreme Traveler</a>
+                    <div className="collapse navbar-collapse " id="navbarTogglerDemo03">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
+                            <li className="nav-item m-2 p-2 text-decoration-none">
+                                <NavLink className="text-decoration-none" to="/home">Home</NavLink>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Offers</a>
+                            <li className="nav-item m-2 p-2">
+                                <NavLink className="text-decoration-none" to="/offers">Offers</NavLink>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Disabled</a>
+                            <li className="nav-item m-2 p-2">
+                                <NavLink className="text-decoration-none" to="/offers">Offers</NavLink>
+                            </li>
+                            <li className="nav-item m-2 p-2">
+                                <NavLink className="text-decoration-none" to="/offers">Offers</NavLink>
+                            </li>
+                            <li className="nav-item m-2 p-2">
+                                <NavLink className="text-decoration-none" to="/offers">Login</NavLink>
                             </li>
                         </ul>
                     </div>
