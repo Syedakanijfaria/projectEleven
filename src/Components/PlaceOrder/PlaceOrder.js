ce37fafda890a22f88489db041241dea54982b38
SKFaria <fariafaria737@gmail.com>
2021-10-31 18:43:02 +0600

    crying

--- src/Components/PlaceOrder/PlaceOrder.js
+++ src/Components/PlaceOrder/PlaceOrder.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import useAuth from '../../Hooks/useAuth.js';
+import axios from 'axios';
 
 const PlaceOrder = () => {
 
@@ -13,6 +14,7 @@ const PlaceOrder = () => {
             .then(res => res.json())
             .then(data => setDetails(data))
         , [id])
+
     // useEffect(() => {
     //     if (details.length > 0) {
     //         const matchedData = details.find(detail => detail.key === id)
@@ -20,14 +22,22 @@ const PlaceOrder = () => {
     //     }
 
     // }, [details])
+
     const { user } = useAuth();
     const { displayName, email } = user;
     const { register, handleSubmit, reset } = useForm();
     const history = useHistory();
+
     const onSubmit = data => {
         console.log(data);
-        const uri = `/myOrder/data`;
-        history.push(uri);
+        axios.post('http://localhost:5000/users', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    reset();
+                }
+            })
+        // const uri = `/myOrder/data`;
+        // history.push(uri);
     }
     return (
         <div className="container">
@@ -48,16 +58,18 @@ const PlaceOrder = () => {
                 <div className="col-sm-12 col-md-6 col-lg-6">
                     <div className="">
                         <form onSubmit={handleSubmit(onSubmit)}>
-                            <p>Tour Tilte: <input {...register("name")} defaultValue={details?.name} placeholder="title" /></p><br />
-                            <p>User Name: <input type="text" {...register(" name")} defaultValue={displayName} /></p><br />
-                            <p>Gender: <select {...register("gender")}>
+                            <p>Tour Tilte: <input {...register("Title", { required: true })} placeholder="Title" /></p><br />
+                            <p>User Name: <input type="text" {...register("Name")} defaultValue={displayName}
+                            /></p><br />
+                            <p>Gender: <select {...register("Gender")}>
                                 <option value="female">female</option>
                                 <option value="male">male</option>
                                 <option value="other">other</option>
                             </select></p><br />
-                            <p>Mail:<input type="email" {...register("email")} defaultValue={email} /></p><br />
-                            <p>Price: $<input type="number" {...register("price")} defaultValue={details?.price} /></p><br />
-                            <p>Mobile Number: <input type="Mobile number" {...register("Mobile number", { required: true })} placeholder="Mobile number" /></p><br />
+                            <p>Mail:<input type="email" {...register("Email")} defaultValue={email} /></p><br />
+                            <p>Price: $<input type="number" {...register("Price", { required: true })} /></p><br />
+                            <p>Departure Date: <input type="date" {...register("Date", { required: true })} placeholder="Departure Date" /></p><br />
+                            <p>Mobile Number: <input type="Mobile number" {...register("Number", { required: true })} placeholder="Mobile Number" /></p><br />
                             <input type="submit" value="Book Now" />
                             {/* <Link to="/myOrder">
                                 <input type="submit" value="Book Now" />
